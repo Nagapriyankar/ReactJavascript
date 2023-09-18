5602a83451ff2a2446e8c7730c61cf379ec02c92
Nagapriyankar <nagapriyankar@gmail.com>
2023-09-17 18:58:07 -0700

    multiple components, adding styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import './style.css'
+
 //OUR FIRST REACT COMPONENT
 //js function
 //for now doesnot receive any arg
+
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Greeting called</p>
+      <p>Hello {props.name}, your age is {props.age}</p>
+    </div>
+  )
+}
 
 const App = () => { 
   /* 
@@ -18,7 +30,10 @@ const App = () => {
   return (
     <div>
       <p>Hello World! it is {now.toString()}</p>
-      <p>Hello { mname } is {a + b } years old</p>
+      <p>Hello {mname} is {a + b} years old</p>
+      <h1 style={{background: 'red', color:'white'}}>Greetings</h1>
+      <Hello name='sathish' age='25'/>
+      <Hello name='Krish' age='15'/>
     </div>
   )
 }
