88f411f27ef5b1dba89ea063b50f9c1a030f98f2
Nagapriyankar <nagapriyankar@gmail.com>
2023-09-24 17:22:40 -0700

    statful - class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,32 @@
-/* import React from 'react'
-
-//pasing props functional component
-function Hello({name}) {
-  return (
-    <div>
-      Hello, {name}
-    </div>
-  )
-}
-
-function App() {
-  return (
-    <div>
-      <Hello name="KRISH" />
-    </div>
-  )
-} */
-
-
 // class component
 
 import { Component } from "react"
 
-class Hello extends Component {
-  render() {
-    const { name } = this.props;
-    return <div>Hello {name}!</div>
-  }
-}
-
+//stateful components
 class App extends Component {
+  constructor(props) { 
+    super(props); //to call parent class constructor
+
+    //define the state
+    this.state = {
+      count:0,
+    }
+  }
+
+  increment = () => { 
+    //update the state (data) of the count
+    this.setState(
+      {
+        count: this.state.count+1,
+      }
+    )
+  }
+
   render() { 
     return (
       <div>
-        <Hello name='Priyanka' />
+        <p>Count: {this.state.count}</p>
+        <button onClick={ this.increment }>INCREMENT</button>
       </div>
     )
   }
